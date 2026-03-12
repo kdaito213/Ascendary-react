58dcc669b82f1ac0ef0f5f2f867aa5b3947b8e92
kdaito213 <kdaito213@gmail.com>
2026-03-12 16:07:01 +0900

    feature: consecutiveDay

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -9,26 +9,60 @@ function Dashboard({record}){
                 todayRecord.push(record[i])
             }
         }
-
         return todayRecord
+    }
+
+    function getDates(){
+        let dates=[]
+        for(let i=0;i<record.length;i++){
+            if(!dates.includes(record[i].date)){
+            dates.push(record[i].date)
+            }
+        }
+        dates.sort().reverse()
+        return dates
+    }
+
+    let dates = getDates()
+
+    function consecutiveDays(dates){
+        let consecutiveDaysCount = 0
+        let day = new Date()
+        while(true){
+            let strDay = day.toISOString().slice(0,10)
+            
+            if(dates.includes(strDay)){
+                consecutiveDaysCount++
+                day.setDate(day.getDate()-1)
+            }else{
+                break
+            }
+        }
+
+        return consecutiveDaysCount
     }
 
     let todayRecord = todayRecords()
 
-    let list = []
+    let todayList = []
     for (let i=0; i<todayRecord.length; i++) {
-                list.push(
-                    <div key={i}>
-                    {todayRecord[i].name}：{todayRecord[i].score}回
-                    </div>
-                )    
-            }
+        todayList.push(
+            <div key={i}>
+                {todayRecord[i].name}：{todayRecord[i].score}回
+            </div>
+        )    
+    }
+
+    let consecutiveDay = consecutiveDays(dates)
 
     return (    
         <div className="dashboard-card"> 
             <h1>今日の記録</h1>
-            {list}
+            {todayList}
             {todayRecord.length === 0 && <p>今日の記録はありません</p>}
+            <h1>連続記録日数</h1>
+            <p>連続記録日数は{consecutiveDay}日です</p>
+
         </div>
     )
 }
