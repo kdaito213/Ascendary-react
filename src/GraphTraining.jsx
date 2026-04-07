1545a32f66726b712dae55d99a718234f2cfd032
kdaito213 <kdaito213@gmail.com>
2026-04-07 12:20:25 +0900

    fix: css

--- src/GraphTraining.jsx
+++ src/GraphTraining.jsx
@@ -31,26 +31,26 @@ function GraphTraining({record}) {
         <h2>グラフ</h2>
 
         {skills.map((skill) => (
-            <div key={skill}>
+            <div key={skill} style={{marginBottom: "30px"}}>
                 <h3>{skill}</h3>
+                <div style={{ width: "100%", height: "250px" }}>
+                    <ResponsiveContainer>
+                        <LineChart
+                            data={makeGraphDataBySkill(record, skill)}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="date" />
+                            <YAxis />
+                            <Tooltip />
 
-                <LineChart
-                    width={600}
-                    height={300}
-                    data={makeGraphDataBySkill(record, skill)}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="date" />
-                    <YAxis />
-                    <Tooltip />
-
-                    <Line
-                        type="monotone"
-                        dataKey="score"
-                        stroke="#8884d8"
-                    />
-                </LineChart>
-
+                            <Line
+                                type="monotone"
+                                dataKey="score"
+                                stroke="#8884d8"
+                            />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </div>
             </div>
         ))}
         </div>
